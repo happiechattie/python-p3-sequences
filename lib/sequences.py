7fefd95232b1744a498fde1d3cd9d041136e050e
Christine Baik <christuaik@gmail.com>
2023-08-03 09:47:31 -0700

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    n1 = 0
+    n2 = 1
+    n3 = n1 + n2
+    l = []
+    for n in range(length):
+        l.append(n1)
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+    print(l)
